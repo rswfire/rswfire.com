6bcc25f5be5781d6851b9b9609414aeb73230816
Robert Samuel White <rsw@rswfire.online>
2025-04-20 19:42:57 -0700

    Updated Configuration

--- config/auth.php
+++ config/auth.php
@@ -9,13 +9,13 @@ return [
     |
     | This option defines the default authentication "guard" and password
     | reset "broker" for your application. You may change these values
-    | as required, but they're a perfect start for most applications.
+    | as required, but they"re a perfect start for most applications.
     |
     */
 
-    'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+    "defaults" => [
+        "guard" => env("AUTH_GUARD", "web"),
+        "passwords" => env("AUTH_PASSWORD_BROKER", "users"),
     ],
 
     /*
@@ -35,10 +35,10 @@ return [
     |
     */
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
+    "guards" => [
+        "web" => [
+            "driver" => "session",
+            "provider" => "users",
         ],
     ],
 
@@ -59,15 +59,15 @@ return [
     |
     */
 
-    'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+    "providers" => [
+        "users" => [
+            "driver" => "eloquent",
+            "model" => env("AUTH_MODEL", App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
+        // "users" => [
+        //     "driver" => "database",
+        //     "table" => "users",
         // ],
     ],
 
@@ -76,7 +76,7 @@ return [
     | Resetting Passwords
     |--------------------------------------------------------------------------
     |
-    | These configuration options specify the behavior of Laravel's password
+    | These configuration options specify the behavior of Laravel"s password
     | reset functionality, including the table utilized for token storage
     | and the user provider that is invoked to actually retrieve users.
     |
@@ -90,12 +90,12 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
+    "passwords" => [
+        "users" => [
+            "provider" => "users",
+            "table" => env("AUTH_PASSWORD_RESET_TOKEN_TABLE", "password_reset_tokens"),
+            "expire" => 60,
+            "throttle" => 60,
         ],
     ],
 
@@ -110,6 +110,6 @@ return [
     |
     */
 
-    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+    "password_timeout" => env("AUTH_PASSWORD_TIMEOUT", 10800),
 
 ];
