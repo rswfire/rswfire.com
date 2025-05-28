cc4ceda3a5c89964aa3fc4e78d3184cdd1b6844d
Robert Samuel White <rsw@rswfire.online>
2025-05-27 22:24:24 -0700

    Added Analytics

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,21 @@
 import { createApp } from "vue";
 import App from "./pages/App.vue";
 import "../css/app.css";
-import router from './router'
+import router from './router';
+import VueGtag from 'vue-gtag-next';
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+const gtagId = 'G-5K6H8GR1Q7';
+
+app.use(router);
+app.use(VueGtag, {
+    property: {
+        id: gtagId,
+    },
+    config: {
+        send_page_view: true
+    }
+});
+
+app.mount("#app");
