517fc2afda0a8fbee784b5b2a15686abceee9a80
Robert Samuel White <rsw@rswfire.online>
2025-05-27 22:16:45 -0700

    Added Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ request()->getHost() }}</title>
     @vite(["resources/css/app.css", "resources/js/app.js"])
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5K6H8GR1Q7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-XXXXXXXXXX');
+    </script>
 </head>
 <body>
 <div id="app"></div>
