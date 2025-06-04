b5a0776f8ef378b9d126252dbb5c79ceec07a366
Robert Samuel White <rsw@rswfire.online>
2025-06-03 22:37:34 -0700

    Created Myth Page

--- resources/js/components/menu/Navigation.vue
+++ resources/js/components/menu/Navigation.vue
@@ -26,7 +26,7 @@
             </router-link>
 
             <router-link
-                to="/the-journey"
+                to="/myth"
                 class="flex items-center gap-2 hover:text-red-600 transition"
             >
                 <span>🌪️</span> <span>The Myth</span>
