8fd3a9d5cd921b7642c290d685ecfd23b4233052
Robert Samuel White <rsw@rswfire.online>
2025-06-11 17:48:12 -0700

    Fixed Issue with Ziggy Reference

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,8 @@ import './bootstrap';
 
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
 import DefaultLayout from './Layouts/DefaultLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
