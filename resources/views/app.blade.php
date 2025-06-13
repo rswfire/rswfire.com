0729f511418d62681cd106ce6caadbde58bb8498
Robert Samuel White <rsw@rswfire.online>
2025-06-13 00:20:38 -0700

    Re-Added Google Analytics Tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5K6H8GR1Q7"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5K6H8GR1Q7');
+        </script>
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js'])
