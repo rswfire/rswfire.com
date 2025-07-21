5ff8e7b52a4e400c2b0fc767f1f060cc3da529bd
Robert Samuel White <rsw@rswfire.online>
2025-07-20 21:53:57 -0700

    Updated Mobile Menu

--- resources/js/Components/Menu/Panel.vue
+++ resources/js/Components/Menu/Panel.vue
@@ -21,26 +21,26 @@
 
             <div class="w-full flex justify-center">
                 <div class="flex items-center space-x-2">
-                    <Link href="/about" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/about" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         <Icon name="MessageCircle" color="text-black" class="w-4 h-4 flex-shrink-0" />
                     </Link>
-                    <Link href="/about" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/about" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         About
                     </Link>
 
                     <div class="space-x-4">&nbsp;</div>
-                    <Link href="/login" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/login" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         <Icon name="LogIn" color="text-black" class="w-4 h-4 flex-shrink-0" />
                     </Link>
-                    <Link href="/login" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/login" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         Sign In
                     </Link>
 
                     <div class="space-x-4">&nbsp;</div>
-                    <Link href="/register" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/register" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         <Icon name="UserPlus" color="text-black" class="w-4 h-4 flex-shrink-0" />
                     </Link>
-                    <Link href="/register" class="text-sm font-medium text-gray-700 hover:underline">
+                    <Link href="/register" @click="$emit('close')" class="text-sm font-medium text-gray-700 hover:underline">
                         Register
                     </Link>
                 </div>
