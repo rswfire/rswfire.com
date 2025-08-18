1391ac2089a35fc4a3cb236b04e13180d0154929
Robert Samuel White <rsw@rswfire.online>
2025-08-17 23:09:03 -0700

    Updated Honeyman Section

--- resources/js/Pages/Honeyman/Sections/Footer.vue
+++ resources/js/Pages/Honeyman/Sections/Footer.vue
@@ -1,16 +1,19 @@
 <template>
     <section id="page-footer" class="pt-6 border-t border-gray-300 text-black">
 
-        <div>This page is not for revenge.</div>
+        <div class="text-xs font-bold">This is the map. This is the pattern. This is the mirror.</div>
+
+        <div class="mt-4">This page is not for revenge.</div>
         <div>It is for those who have been told they imagined it.</div>
         <div>It is for those about to walk into something similar.</div>
         <div>It is for the future, when denial no longer holds.</div>
 
         <div class="mt-4">It does not ask for apology.</div>
         <div>It does not ask for repair.</div>
-        <div>It exists <span class="font-semibold">so that the next distortion cannot pretend it was the first.</span></div>
+        <div>It exists so that the next distortion cannot pretend it was the first.</div>
 
     </section>
 </template>
 
-<script></script>
+<script>
+</script>
