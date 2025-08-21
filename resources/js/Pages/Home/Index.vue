7b597accaeb354cc7e49b5ad0b614a00d61176f6
Robert Samuel White <rsw@rswfire.online>
2025-08-21 16:50:35 -0700

    Updated Home/Index Page

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -7,7 +7,7 @@
             <div class="md:col-span-2 flex flex-col">
                 <div class="flex flex-col">
                     <!-- <CompanionCard class="mb-6 sm:mb-6 md:mb-0 md:hidden" /> -->
-                    <!-- <FieldcraftRecentCard :entries="recentFieldcraft" class="mb-6 sm:mb-6 md:mb-0 md:hidden" /> -->
+                    <FieldcraftRecentCard :entries="recentFieldcraft" class="mb-6 sm:mb-6 md:mb-0 md:hidden" />
                     <HelloCard class="mb-6 sm:mb-6" />
                     <TechCard />
                 </div>
@@ -16,8 +16,8 @@
             <div class="md:col-span-1">
                 <div class="flex flex-col">
                     <!-- <CompanionCard class="hidden md:block" /> -->
-                    <!-- <FieldcraftRecentCard :entries="recentFieldcraft" class="hidden md:block md:mt-6" /> -->
-                    <CodexCard />
+                    <FieldcraftRecentCard :entries="recentFieldcraft" />
+                    <CodexCard class="hidden md:block md:mt-6" />
                 </div>
             </div>
 
