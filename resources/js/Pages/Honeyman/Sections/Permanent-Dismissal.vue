780f9249b7f7b60e156cd7f42c9b73948ca87a54
Robert Samuel White <rsw@rswfire.online>
2025-08-22 16:04:37 -0700

    Updated Honeyman/*

--- resources/js/Pages/Honeyman/Sections/Permanent-Dismissal.vue
+++ resources/js/Pages/Honeyman/Sections/Permanent-Dismissal.vue
@@ -53,7 +53,8 @@
                 <div class="mt-4">Allison Watson</div>
             </div>
 
-            <div class="mt-4 text-center"><a href="/oprd-dismissal.pdf" download>Download the Dismissal Letter (PDF)</a></div>
+            <div class="mt-4 text-center font-bold italic">This is the loop: dismissed, then punished for naming the dismissal. A logic so thin it reveals itself.</div>
+            <div class="text-center"><a class="text-honeyman-600 underline" href="/oprd-dismissal.pdf" download>Download the Dismissal Letter (PDF)</a></div>
 
         </div>
 
