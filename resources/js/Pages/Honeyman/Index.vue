71284c43d7e52dec4bd19e9893c9e756900c148c
Robert Samuel White <rsw@rswfire.online>
2025-08-26 22:14:53 -0700

    Updated Honeyman/*

--- resources/js/Pages/Honeyman/Index.vue
+++ resources/js/Pages/Honeyman/Index.vue
@@ -5,7 +5,7 @@
         <Hero
             title="THE HONEYMAN ARCHIVE"
             subtitle="HONEYMAN STATE PARK, OREGON PARKS &amp; RECREATION DEPARTMENT (OPRD)"
-            meta="LAST UPDATED: 08/26/25"
+            meta="THE TREATMENT OF VOLUNTEERS IS NOT PERIPHERAL BUT CENTRAL TO INSTITUTIONAL LEGITIMACY"
         />
 
         <div class="mt-8 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-black">
