6e2789dbf29b46daf7436db8ae27b9f6249bd0dd
Robert Samuel White <rsw@rswfire.online>
2025-08-27 19:06:59 -0700

    Updated Honeyman/*

--- resources/js/Pages/Honeyman/Sections/Header.vue
+++ resources/js/Pages/Honeyman/Sections/Header.vue
@@ -6,7 +6,7 @@
 
             <div class="mt-4">This archive is not a monument to survival. It is a demand for <strong>structural protection</strong>.</div>
             <div class="mt-4">I built this because no one should endure <strong>systematic institutional abuse</strong> while serving without compensation &mdash; unpaid, unsupported, undocumented &mdash; in silence, in isolation, in fear of reprisal.</div>
-            <div class="mt-4">Until Oregon State Parks creates real, enforceable protections for volunteers, this archive remains open, active, and expanding. When institutions depend on unpaid labor but offer no recourse when harm occurs, they create conditions for <strong>ongoing abuse</strong>. When that abuse is named and met with silence, that silence becomes the next act of harm.</div>
+            <div class="mt-4">Until Oregon State Parks creates real, enforceable protections for volunteers, this archive remains open, active, and expanding. When institutions depend on unpaid labor but offer no recourse when harm occurs, they create conditions for <strong>ongoing abuse</strong>. When that abuse is named and met with silence, that silence becomes <strong>the next act of harm</strong>.</div>
             <div class="mt-4">This is not about retribution. This is about <strong>systemic integrity</strong>.</div>
             <div class="mt-4">It is here so others don't get erased. It is here so the next whistleblower isn't isolated. It is here because someone has to hold the line.</div>
             <div class="mt-4 italic font-bold">&quot;They tried to erase me, so I became unerasable, and I won't let them erase you either.&quot;</div>
