21e7da6790c486e33313cb70cccbf51bcbb473d1
Robert Samuel White <rsw@rswfire.online>
2025-08-30 19:32:20 -0700

    Updated Layout/*

--- resources/js/Pages/Honeyman/Sections/Footer.vue
+++ resources/js/Pages/Honeyman/Sections/Footer.vue
@@ -13,7 +13,25 @@
         <div>It exists so that the next distortion cannot pretend it was the first.</div>
 
         <div class="mt-4"><em>I held my shape. Now hold yours</em>.</div>
-        <div class="mt-4">🙃</div>
+        <div class="mt-4">
+          <span class="relative inline-flex items-center group">
+          <span
+              tabindex="0"
+              aria-describedby="emoji-tip"
+              class="text-xl leading-none select-none"
+          >🙃</span>
+          <span
+              id="emoji-tip"
+              role="tooltip"
+              class="pointer-events-none absolute left-1/2 -translate-x-1/2 bottom-full mb-2
+                   px-2 py-1 rounded-md bg-gray-900 text-white text-xs shadow-lg
+                   opacity-0 group-hover:opacity-100 group-focus-within:opacity-100
+                   transition-opacity duration-150 whitespace-nowrap z-50"
+          >
+            Yes, they even pathologized my use of emojis!
+          </span>
+          </span>
+        </div>
 
     </section>
 </template>
