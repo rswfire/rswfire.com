f6441fb7f4bc1f471c7660ad2d666d103e5c4e5a
Robert Samuel White <rsw@rswfire.online>
2025-10-22 19:52:28 -0700

    Structural Updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     build: {
-        outDir: '../public/assets',
+        outDir: 'public/assets',
         emptyOutDir: true,
         manifest: true,
     },
